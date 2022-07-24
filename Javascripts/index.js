aa5638aa7ed95c5b7d5b1b77770a063423937553
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-07-24 17:38:54 -0400

    trouble shooting JSON

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -1,6 +1,6 @@
 //**Global Variables *//
-const baseURL = 'http://localhost:3000'
-let anime = []; 
+const baseURL = 'http://localhost:3000/animes'
+let allTheAnimes = [];
 
 //**Node Getters *//
 const dropDown1 = document.getElementById ('inputGroupSelect01'); 
@@ -14,13 +14,9 @@ const submitButton = document.getElementById('submit');
 
 const displayBox = document.getElementById('display-panel');
 
-
-
 //**Start-Up*//
 questionTwo.style.display = 'none';  
 questionThree.style.display = 'none';
-// displayBox.display = allTheAnimes; 
-
 
 //**Event Listeners  *//
 // document.addEventListener('DOMContentLoaded', function(){
@@ -40,38 +36,81 @@ dropDown2.addEventListener('click', function(event){
     }
 });
 
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault(); 
 submitButton.addEventListener('click', function(event){
     if(! ((dropDown2.value == '0' || dropDown3.value =='0' || dropDown1.value == '0'))){   
-        console.log("Good") 
-
-        // allTheAnimes = fetch("localhost:3000")
+        console.log("Good")        
         // animeToDisplay = allTheAnimes.value.filter("morning")
         // anime.style.display = "inject animeToDisplay";
         // filter through all the animes and display
     } else {
         alert("You must complete all three Questions To Get Your Anime!");
     }
-})    
+});
+getAnime(); 
+});
 
+ 
 //**Event Handlers *//
-const loadAnime = event => {
-    event.preventDefault()
+// const loadAnime = event => {
+//     event.preventDefault()
 
-    animes.forEach(anime => {
+//     animes.forEach(anime => {
         
-    });
-}
+//     });
+// }
 
-//**Requests *//
-const allTheAnimes = () => {
-    fetch(baseURL + "./animes")
-        .then(resp => resp.json())
-        .then(data => {
-            console.log('data', data)
-            anime = data
-        })
+function showAnime(anime) { 
+    const div = document.createElement('div');
+    div.classList.add("card")
+    const h2 = document.createElement('h2');
+    h2.textContent = anime.title
+    const h3 = document.createElement('h3');
+    h3.textContent = anime.description
+
+    const img = document.createElement('img')
+    img.src = anime.poster
+    img.classList.add("anime-avatar")
+
+    // const button = document.createElement('button')
+    // button.className.add("add-anime");
+    // button.id = anime.id
+
+    div.append(h2, h3, img)
+    displayBox.append(div)
 }; 
 
+
+//**Requests *//
+// const allTheAnimes = () => {
+//     fetch(baseURL + "./animes")
+//         .then(resp => resp.json())
+//         .then(data => {
+//             console.log('data', data)
+//             anime = data
+//         })
+// }; 
+
+// function getAnime() {
+//     fetch(baseURL)
+//     .then(response => response.json())
+//     .then(data =>{
+//       console.log("data", data)
+//     })
+//   }; 
+//         data.forEach(anime => showAnime(anime)))
+//   }; 
+
+async function getAnime() {
+    const response = await fetch(baseURL)
+    const data = await response.json();
+    allTheAnimes = data; 
+}; 
+
+
+ 
 //**Node Creators *//
 //**MISC *//
 
