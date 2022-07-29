e0e78c7fe61abf43c2cfd77f9817de7502c2a95a
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-07-28 21:15:10 -0400

    JS complete

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -2,7 +2,7 @@
 const digiAPI = `https://digimon-api.vercel.app/api/digimon`;
 let allDigimons = [];
 let digimonStorage = [];
-// let currentDigimon = '';
+let currentDigimon = '';
 
 // Node Getters
 const digiDisplay = document.getElementById('digi-main');
@@ -21,56 +21,65 @@ async function loadDigi(userInput) {
     const response = await fetch(digiAPI);
     const data = await response.json();
     allDigimons = data;
-    
+    let digimonFound = false
+
     for (const digimon of allDigimons) {
-        
+
         if (digimon.name.toUpperCase() === userInput.toUpperCase()) {
             // console.log(digimon);
             const previewBox = document.createElement('div');
             const digiModel = document.createElement('img');
             const digiName = document.createElement('h1');
             const digiLevel = document.createElement('h3');
-            
+
             digiName.innerText = digimon.name;
             digiModel.src = digimon.img;
             digiLevel.innerText = digimon.level;
-            
+
             digiDisplay.innerHTML = '';
 
             digiDisplay.append(previewBox);
-            
+
             previewBox.append(digiName);
             // document.createElement('br');
             previewBox.append(digiModel);
             // document.createElement('br');
             previewBox.append(digiLevel);
-            
+
             // console.log(digimon.name);
             // console.log(digimon.level);
-            
-            // currentDigimon = digimon;
-            digimonStorage.push(digimon);
+
+            currentDigimon = digimon;
+            // digimonStorage.push(digimon);
+            digimonFound = true;
+
             console.log(digimonStorage);
-        } 
+        }
+
     }
+
+    if (!digimonFound) {
+        alert('no digimon found')
+    }
+
 }
 // };
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', function () {
-    loadDigi();
-    addButton(); 
-    deleteBttn(); 
+    // loadDigi();
+    addButton();
+    deleteBttn();
 });
 
 searchBttn.addEventListener('click', e => {
     loadDigi(searchBox.value);
     searchBox.value = '';
-    document.addEventListener('submit', function(){
-        loadDigi(digimon.name)
+    document.addEventListener('submit', function () {
+        // loadDigi(digimon.name)
         form.reset()
     })
-}); 
+});
 
 const addButton = () => addDigi.addEventListener('click', e => {
     // const currentDigimon = digimonStorage.length-1
@@ -80,129 +89,79 @@ const addButton = () => addDigi.addEventListener('click', e => {
     console.log(digimonStorage);
 });
 
-const deleteBttn = () =>deleteDigi.addEventListener('click', e => { 
+const deleteBttn = () => deleteDigi.addEventListener('click', e => {
     deleteDigiFromTeam();
-    console.log(digimonStorage);  
+    console.log(digimonStorage);
 })
 
 
 // Event Handler 
-function addDigiToTeam(){
-    let member = digimonStorage[digimonStorage.length-1]
-    
+function addDigiToTeam() {
+    digimonStorage.push(currentDigimon);
+
+    let member = digimonStorage[digimonStorage.length - 1]
+
     const digiModel = document.createElement('img');
     const digiName = document.createElement('h1');
     const digiLevel = document.createElement('h3');
-    const removeBtn = document.createElement('button');
-    
+
     // removeBtn.addEventListener("click", (e) =>removeMember(e.target))
     // removeBtn.innerText = "Delete from Memory"
-    
+
     digiName.innerText = member.name;
     digiModel.src = member.img;
     digiLevel.innerText = member.level;
-    
+
     const loadOne = document.getElementById('load1');
     const loadTwo = document.getElementById('load2');
     const loadThree = document.getElementById('load3');
-    
-    switch(""){
+
+    if (loadThree.innerHTML) {
+        alert('Only Three!')
+    }
+
+    switch ("") {
         case loadOne.innerHTML:
             loadOne.appendChild(digiName)
             loadOne.appendChild(digiModel)
             break
-        
+
         case loadTwo.innerHTML:
             loadTwo.appendChild(digiName)
             loadTwo.appendChild(digiModel)
             break
-        
+
         case loadThree.innerHTML:
-                loadThree.appendChild(digiName)
-                loadThree.appendChild(digiModel)
-                break
+            loadThree.appendChild(digiName)
+            loadThree.appendChild(digiModel)
+            break
         default:
     }
 };
 
-function deleteDigiFromTeam(){
+function deleteDigiFromTeam() {
 
-const loadOne = document.getElementById('load1');
-const loadTwo = document.getElementById('load2');
-const loadThree = document.getElementById('load3');   
+    const loadOne = document.getElementById('load1');
+    const loadTwo = document.getElementById('load2');
+    const loadThree = document.getElementById('load3');
 
-console.log(loadThree.innerHTML)
-console.log(loadTwo.innerHTML)
-console.log(loadOne.innerHTML)
+    console.log(loadThree.innerHTML)
+    console.log(loadTwo.innerHTML)
+    console.log(loadOne.innerHTML)
 
-if (loadThree.innerHTML){ 
-    loadThree.replaceChildren();
-    digimonStorage.pop();
-    console.log('check three')
+    if (loadThree.innerHTML) {
+        loadThree.innerHTML = '';
+        digimonStorage.pop();
+        console.log('check three')
 
-} else if(loadTwo.innerHTML ){ 
-    loadTwo.replaceChildren();
-    digimonStorage.pop();
-    console.log('check three')
+    } else if (loadTwo.innerHTML) {
+        loadTwo.innerHTML = '';
+        digimonStorage.pop();
+        console.log('check three')
 
-} else if(loadOne.innerHTML ){ 
-    loadOne.replaceChildren();
-    digimonStorage.pop();
-    console.log('check three')
-}
-
-    // switch(""){
-    //     case loadThree.innerHTML:
-    //         loadThree.innerHTML = " "
-    //         digimonStorage.pop();
-    //         break
-        
-    //     case loadTwo.innerHTML:
-    //         loadTwo.innerHTML = " "
-    //         digimonStorage.pop();
-    //         break
-        
-    //     case loadOne.innerHTML:
-    //             loadOne.innerHTML = " "
-    //             digimonStorage.pop();
-    //             break
-    //     default:
-    // } 
+    } else if (loadOne.innerHTML) {
+        loadOne.innerHTML = '';
+        digimonStorage.pop();
+        console.log('check three')
+    }
 };
-
-function showError(){
-
-};
-
-function limitMessage(){
-
-}; 
-
-
-// function addDigiToTeam(digimonToBeAdded) {
-//     loadDigi(digimonToBeAdded);
-    
-//     digimonStorage.id = digimonToBeAdded;
-    
-//     const previewBox = document.createElement('div');
-    
-//     const digiModel = document.createElement('img');
-//     const digiName = document.createElement('h1');
-//     const digiLevel = document.createElement('h3');
-
-//     digiName.innerText = digimonToBeAdded.name;
-//     digiModel.src = digimonToBeAdded.img;
-//     digiLevel.innerText = digimonToBeAdded.level;
-
-//     digiHolder.append(previewBox);
-
-//     previewBox.append(digiName);
-//     previewBox.append(digiModel);
-//     previewBox.append(digiLevel);
-//     previewBox.append(deleteDigi);
-// };
-
-// function removeDigiFromTeam(digimonToBeDeleted) {
-//     console.log(digimonToBeDeleted)
-//     document.getElementById(digimonToBeDeleted.toUpperCase()).remove();
-// };
